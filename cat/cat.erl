1f24fd589a97ff39cd79b6143b6632bde6d780ec
chuck <ha.chuck@gmail.com>
2012-11-04 16:16:28 -0500

    Make sure to understand your datastructures
    All tests pass.
    
    A file is a list of lines
    A line is a list of chars

--- cat/cat.erl
+++ cat/cat.erl
@@ -20,8 +20,10 @@ parse_args_caller(Args) ->
 parse_args([Arg|Rest], Files, Flags) ->
     %% If it is not a valid arg, assume it is a file
     case lists:member(Arg, ?VALID_FLAGS) of
-        true -> parse_args(Rest, Files, [Arg | Flags]);
-        false -> parse_args(Rest, [Arg | Files], Flags)
+        true -> 
+            parse_args(Rest, Files, [Arg | Flags]);
+        false -> 
+            parse_args(Rest, [Arg | Files], Flags)
     end;
 parse_args([], Files, Flags) ->
     {ok, {filenames, lists:reverse(Files)}, {flags, Flags}}.
@@ -33,40 +35,43 @@ usage() ->
 
 
 cat(Filenames, Flags) ->
+    {ok, {files, Files}} = read_files(Filenames, []),
+    print_files(Files, Flags).
+
+read_files([], Files) ->
+    {ok, {files, lists:reverse(Files)}};
+read_files([Filename|Others], Files) ->
+    {ok, IoDevice} = file:open(Filename, [read]),
+    {ok, {lines, Lines}} = gather_lines(IoDevice, []),
+    read_files(Others, [Lines|Files]).
+
+gather_lines(IoDevice, Lines) ->
+    case file:read_line(IoDevice) of
+        {ok, Line} ->
+            gather_lines(IoDevice, [Line|Lines]);
+        eof ->
+            {ok, {lines, lists:reverse(Lines)}}
+    end.
+
+print_files([], _) ->
+    ok;
+print_files([File|Files], Flags) ->
+    print_lines(File, Flags, 1),
+    print_files(Files, Flags).
+
+print_lines([], _, _) ->
+    ok;
+print_lines([Line|Lines], Flags, LineCount) ->
     case lists:member("-n", Flags) of
-        true -> b_read_files(Filenames);
-        false -> read_files(Filenames)
-    end.
+        true -> 
+            print_n_line(Line, LineCount);
+        false ->
+            print_line(Line)
+    end,
+    print_lines(Lines, Flags, LineCount + 1).
 
-b_read_files([]) ->
-    ok;
-b_read_files([Filename|Others]) ->
-    {ok, IoDevice} = file:open(Filename, [read]),
-    b_print_line(IoDevice, 1),
-    b_read_files(Others).
+print_line(Line) ->
+    io:format("~s", [Line]).
 
-read_files([]) ->
-    ok;
-read_files([Filename|Others]) ->
-    {ok, IoDevice} = file:open(Filename, [read]),
-    print_line(IoDevice),
-    read_files(Others).
-
-b_print_line(IoDevice, LineNumber) ->
-    case file:read_line(IoDevice) of
-        {ok, Line} ->
-            io:format("     ~B~c~s", [LineNumber, 9, Line]),
-            b_print_line(IoDevice, LineNumber + 1);
-        eof ->
-            done
-    end.
-
-print_line(IoDevice) ->
-    case file:read_line(IoDevice) of
-        {ok, Line} ->
-            io:format("~s", [Line]),
-            print_line(IoDevice);
-        eof ->
-            done
-    end.
-
+print_n_line(Line, LineNumber) ->
+    io:format("     ~B~c~s", [LineNumber, 9, Line]).
