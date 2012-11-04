cf2394699ab0f55f51478c188679d5042a3869a0
chuck <ha.chuck@gmail.com>
2012-11-03 20:47:19 -0400

    Initial commit

---
+++ cat/cat.erl
@@ -0,0 +1,30 @@
+#!/usr/bin/env escript
+main(Args) ->
+    io:format("~s", [cat(Args)]);
+    %try
+    %catch
+        %_:_ ->
+            %usage()
+    %end;
+
+main(_) ->
+    usage().
+
+usage() ->
+    io:format("usage: cat filename\n"),
+    halt(1).
+
+cat(Filename) ->
+    {ok, IoDevice} = file:open(Filename, [read]),
+    line_by_line(IoDevice).
+
+
+line_by_line(IoDevice) ->
+    case file:read_line(IoDevice) of
+        {ok, Line} ->
+            io:format("~s", [Line]),
+            line_by_line(IoDevice);
+        eof ->
+            done
+    end.
+
